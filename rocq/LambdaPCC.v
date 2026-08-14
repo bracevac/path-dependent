d48078d8bafd6127d4a0d18657b65b25a7b88255
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-08-14 15:36:52 +0200

    Add LambdaPCC Rocq aggregate

---
+++ rocq/LambdaPCC.v
@@ -0,0 +1,24 @@
+From PathDependent.LambdaPCC Require Export
+  FinFun
+  Syntax
+  Context
+  Typing
+  Runtime
+  StoreStratification
+  RuntimeEquality
+  Valuation
+  CaptureEvidence
+  CaptureAction
+  CaptureStatic
+  CaptureCoercion
+  CaptureWeakening
+  CaptureTyping
+  CaptureInterpretation
+  CaptureAllocation
+  CapturePreservation
+  CaptureSafety
+  CaptureBounds
+  CaptureRegression
+  GeneralPairRegression.
+
+(** The capture-checking LambdaP extension and its complete metatheory. *)
