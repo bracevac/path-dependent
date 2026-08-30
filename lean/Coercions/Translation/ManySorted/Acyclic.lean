429858cbeebde0575d305b543fd87c1b24fd54ff
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-08-30 15:07:26 +0200

    Prove captured DOT translation naturality

--- lean/Coercions/Translation/ManySorted/Acyclic.lean
+++ lean/Coercions/Translation/ManySorted/Acyclic.lean
@@ -2,6 +2,7 @@ import Coercions.Translation.ManySorted.Acyclic.ObjectEncoding
 import Coercions.Translation.ManySorted.Acyclic.ObjectEncodingMetatheory
 import Coercions.Translation.ManySorted.Acyclic.Layout
 import Coercions.Translation.ManySorted.Acyclic.StaticTranslation
+import Coercions.Translation.ManySorted.Acyclic.StaticTranslationMetatheory
 
 /-!
 # Acyclic DOT with captures to many-sorted FC
