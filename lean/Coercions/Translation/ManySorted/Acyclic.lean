81312adff1908866d8931c3a1b10e785248f214e
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-08-30 15:55:15 +0200

    Compile captured DOT computations

--- lean/Coercions/Translation/ManySorted/Acyclic.lean
+++ lean/Coercions/Translation/ManySorted/Acyclic.lean
@@ -10,6 +10,7 @@ import Coercions.Translation.ManySorted.Acyclic.SelectionTranslation
 import Coercions.Translation.ManySorted.Acyclic.SelectionUseTranslation
 import Coercions.Translation.ManySorted.Acyclic.SourceErasure
 import Coercions.Translation.ManySorted.Acyclic.ValueTranslation
+import Coercions.Translation.ManySorted.Acyclic.TermTranslation
 
 /-!
 # Acyclic DOT with captures to many-sorted FC
